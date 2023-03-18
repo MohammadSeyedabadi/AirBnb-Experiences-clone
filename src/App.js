f3392f9a7ecb518e720da278ef561f0ca35f96df
mohammad <mo.se.12369@gmail.com>
2023-03-18 10:30:50 +0330

    #

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import data from './data'
+import './style.css'
 
-function App() {
+export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      {/* <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                location="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            /> */}
+      {/* put it in section so it gives us the oppotubity to give {cards} a class name and do some styling and also make use of styles that we put on section, so we have padding on the left. */}
+      <Hero />
+      <section className="cards-list">{cards}</section>
     </div>
-  );
+  )
 }
-
-export default App;
